fe7fc68593421bf63e9fbad243c3ad61c26a3cdd
William Liang <pineappletwo1@outlook.com>
2024-04-27 03:22:58 +0000

    finished feature events and month

--- src/app/featured-events/page.tsx
+++ src/app/featured-events/page.tsx
@@ -3,12 +3,12 @@ export default function featuredevents(){
     return(
         <div>
             <h1>Tyee programming CC</h1>
-            <div></div>
+            <div className="bg-gray-100 rounded-lg p-4 w-1/4"><p>April 25 - In Person Meeting</p></div>
             <h1>Band</h1>
-            <div></div>
-            <div></div>
+            <div className="bg-gray-100 rounded-lg p-4 w-1/4"><p>June 8 - Ocean Shores</p></div>
+            <div className="bg-gray-100 rounded-lg p-4 w-1/4"><p>April 24 - Jazz Night</p></div>
             <h1>Chinese</h1>
-            <div></div>
+            <div className="bg-gray-100 rounded-lg p-4 w-1/4"><p>April 29 - Flipgrid due</p></div>
         </div>
     )
     
--- src/app/month/page.tsx
+++ src/app/month/page.tsx
@@ -1,14 +1,34 @@
-export default function month(){
+const Month: FC = () => {
+    const date = new Date();
+    const daysInMonth = getDaysInMonth(date.getMonth(), date.getFullYear());
 
     return (
-        <div>
-            <h1>Upcomming Events</h1>
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <h1>Upcoming Events</h1>
             <select>
                 <option>Month</option>
                 <option>Week</option>
                 <option>Day</option>
             </select>
-            
+            <div className="grid grid-flow-col grid-rows-5 gap-1 w-full h-full">
+                {daysInMonth.map((day, index) => (
+                    <div key={index} className="p-2 w-auto h-20">
+                        {day.getDate()}
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
+
+function getDaysInMonth(month: number, year: number): Date[] {
+    const date = new Date(year, month, 1);
+    const days: Date[] = [];
+    while (date.getMonth() === month) {
+        days.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+    }
+    return days;
+}
+
+export default Month;
